f134d079a652f1304ce72a848bbf7c39a97d2786
ird-mehdi <mehdidev.ird@gmail.com>
2025-04-14 15:51:01 +0600

    Remove BookmarkScreen and update import paths in NavBarPresenter
    - Deleted the BookmarkScreen file to streamline the bookmark management UI.
    - Updated the import path in NavBarPresenter to reference the new BookmarkPage instead of the removed BookmarkScreen, ensuring consistency in navigation.

--- lib/presentation/main/presenter/nav_bar_presenter.dart
+++ lib/presentation/main/presenter/nav_bar_presenter.dart
@@ -1,7 +1,7 @@
 import 'package:dua/core/base/base_presenter.dart';
 import 'package:dua/core/utility/utility.dart';
 import 'package:dua/presentation/all_dua/ui/all_dua_page.dart';
-import 'package:dua/presentation/bookmark/ui/bookmark_screen.dart';
+import 'package:dua/presentation/bookmark/ui/bookmark_page.dart';
 import 'package:dua/presentation/home/ui/home_page.dart';
 import 'package:dua/presentation/main/presenter/nav_bar_ui_state.dart';
 import 'package:dua/presentation/memorization/ui/memorization_page.dart';
